2439a1e367ac2481d3d2f6ef6db6a7c6e028869e
unknown <nishanneupane368@gmail.com>
2024-03-20 21:48:23 +0545

    update : prod seed

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -9,16 +9,17 @@ export default function Home() {
     <div className="max-w-[988px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2">
       <div className="relative w-[240px] h-[240px] lg:w-[424px] lg:h-[424px] mb-8 lg:mb-0">
         <Image
-          src={"hero.svg"}
+          src={"/hero1.png"}
           fill
           alt="Hero"
           className="object-contain"
         />
       </div>
       <div className="flex flex-col items-center gap-y-8">
-        <h1 className={"text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center"}>
-          Learn, practice and master new language with Linguify
+        <h1 className={"text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center relative"}>
+          Linguify helps you learn the words, grammar, conversation, and reading of languages visually.
         </h1>
+
         <div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full">
           <ClerkLoading>
             <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
